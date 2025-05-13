28cefc6ada52b39fa1b692a7f6e363f49b737a79
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-05-14 05:14:14 +0600

    lets commit

--- 1st.py
+++ 1st.py
@@ -9,8 +9,18 @@ def root():
 # by default fastapi gives detail not found
 
 #  now we are going to define about
+# @app is the path operator decorator 
+# here operator path
 @app.get('/about')
+# path operation function
 def about():
     return {'title':{'this is the about page'}}
 
- 
+@app.get('/blog/{id}')
+def id(id:int):
+    #  if we dont put int that will be string
+    # fetching blog with id
+    return {'id':{id}}
+# go to http://127.0.0.1:8000/docs to see all the path that have been created
+#  got to http://127.0.0.1:8000/redoc to check all the path as web
+#  to run and reload uvicorn app:app --reload
